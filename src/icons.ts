8e1a9a876964a2e8742d58c4a6a10e094310dde6
Cascade-coding <fahim.real.x@gmail.com>
2025-08-10 12:37:46 +0600

    chore: finish hero

--- src/icons.ts
+++ src/icons.ts
@@ -12,9 +12,12 @@ export const icons: Record<string, (classes?: string) => string> = {
 </defs>
 </svg>
 `,
-   
-
-    // 'x': (classes = '') => ``,
+    'circle': (classes = '') => `<svg class="${classes}" width="600" height="600" viewBox="0 0 600 600" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g opacity="0.2">
+<path d="M300 568.75C448.426 568.75 568.75 448.426 568.75 300C568.75 151.573 448.426 31.25 300 31.25C151.573 31.25 31.25 151.573 31.25 300C31.25 448.426 151.573 568.75 300 568.75Z" stroke="#A5BBA2" stroke-width="16.6667" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+</svg>
+`,
     // 'x': (classes = '') => ``,
     // 'x': (classes = '') => ``,
     // 'x': (classes = '') => ``,
